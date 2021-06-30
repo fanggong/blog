baadb6c71f2de6acfa7ebbe0ec5966b7b7d1a3eb
fanggong <yongchao.fang@outlook.com>
2021-06-30 23:00:56 +0800

    update post

--- public/rmarkdown-libs/flipdownWidgets-binding/flipdownWidgets.js
+++ public/rmarkdown-libs/flipdownWidgets-binding/flipdownWidgets.js
@@ -4,16 +4,29 @@ HTMLWidgets.widget({
   factory: function(el, width, height) {
     return {
       renderValue: function(parameters) {
+
+        el.innerHTML = "";
+
         new FlipDown(parameters.to, el.id, {
           theme: parameters.theme,
           headings: parameters.headings
         })
         .start()
         .ifEnded(() => {
-          console.log("The countter has ended!")
-        })
+          console.log("The counter has ended!")
+        });
+
+        this.resize(width, height);
+
       },
       resize: function(width, height) {
+
+        var width = parseInt(el.offsetWidth);
+        var dayDigit = el.firstChild.childElementCount;
+        var append = 55 * (dayDigit - 3);
+        var baseUnit = width / (510 + append) - 0.01;
+        el.style.setProperty("--base-unit", baseUnit);
+        el.style.setProperty("height", baseUnit * 120 + "px");
 
       }
     };
